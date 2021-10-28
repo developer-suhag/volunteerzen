b34f1271153b70706a5c32d4e69e88d1b84bb785
Suhag Al Amin <developersuhag@gmail.com>
2021-10-28 16:50:54 +0600

    show event by user

--- src/components/Pages/EventsPage/SingleEvent/SingleEvent.js
+++ src/components/Pages/EventsPage/SingleEvent/SingleEvent.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "./SingleEvent.css";
 
 const SingleEvent = ({ event }) => {
-  const { EventName, img } = event;
+  const { EventName, img, Date } = event;
   return (
-    <div>
-      <img src={img} alt="" />
-      <h3>{EventName}</h3>
-    </div>
+    <>
+      <Card className="p-3 rounded-3 shadow-sm">
+        <div className="d-flex justify-content-between gap-3">
+          <Card.Img className="img-fluid event-img shadow-md" src={img} />
+          <Card.Body>
+            <Card.Title className="fw-bold colored-text">
+              {EventName}
+            </Card.Title>
+            <p>
+              <strong>{Date}</strong>
+            </p>
+            <div className="text-end">
+              <Button variant="outline-dark">Cancel</Button>
+            </div>
+          </Card.Body>
+        </div>
+      </Card>
+    </>
   );
 };
 
